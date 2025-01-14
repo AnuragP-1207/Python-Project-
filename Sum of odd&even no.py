4660d076f00e4545021f06cdbbada4c8afa934cf
AnuragP-1207 <113623342+AnuragP-1207@users.noreply.github.com>
2025-01-14 23:45:03 +0530

    Add files via upload

---
+++ Sum of odd&even no.py
@@ -0,0 +1,11 @@
+list_num=[]
+s=0
+print("Enter 10 number ;")
+for i  in range(10):
+   n=int(input())
+   list_num.append(n)
+   if n%2==0:
+      s+=0
+
+print(list_num)
+print("\n sum of all elements in the list ",s) 
