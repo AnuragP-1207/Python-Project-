f3ed1ce8e07cbc3960ec8423f7918c903c4e2ff6
AnuragP-1207 <113623342+AnuragP-1207@users.noreply.github.com>
2025-01-21 23:43:30 +0530

    Add files via upload

---
+++ Area part1.py
@@ -0,0 +1,13 @@
+import math.import pi
+def area_circle(r):
+   a=math.pi*r*r
+   return a
+
+def area_triangle(h,b):
+   a=1/2*b*h
+   return a
+
+def area_rect(l,b):
+   a=l*b
+   return a
+
---
+++ Area part2.py
@@ -0,0 +1,12 @@
+import math
+area_part1
+r=int(input("\nEnter radius of a circle: "))
+print("Area of circle: ",area_part1.area_circle(r))
+
+b=int(input("\nEnter base of the trriangle: "))
+h=int(input("\nEnter height of the trriangle: "))
+print("Area of triangle :",area.area_triangle(h,b))
+
+l=int(input("\nEnter lenght of the rectangle: "))
+b=int(input("\nEnter breath of the rectangle: "))
+print("Area of rectangle :",area.area_rect(l,b))
