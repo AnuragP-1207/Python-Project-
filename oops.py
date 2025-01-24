89eba715ab0c8ccedd8e6a80ec606aab1742974b
AnuragP-1207 <113623342+AnuragP-1207@users.noreply.github.com>
2025-01-24 23:44:30 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+the_car = Car("TATA" , "Safari")
+the_new = Car("MG" , "Hybrid")
+              
+print(the_car.brand)
+print(the_car.model)
+
+print(the_new.brand)
+print(the_new.model)
